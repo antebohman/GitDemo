f6ad86e6918617e040bb9f250c74acb7786728f9
Anton Bohman <ante-2001@live.se>
2024-10-23 11:00:33 +0200

    created method changes

---
+++ badsalt/badsalt/Helpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace badsalt
+{
+    internal class Helpers
+    {
+        public static void WriteText(string text) {
+            Console.WriteLine(text);
+        }
+    }
+}
